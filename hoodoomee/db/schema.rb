36064746df5e66847203a795ab44a5f49a33f4b7
Kareem James <kareemdjames@gmail.com>
2017-12-13 14:45:32 -0500

    created initial favorite model

--- hoodoomee/db/schema.rb
+++ hoodoomee/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213193227) do
+ActiveRecord::Schema.define(version: 20171213194430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20171213193227) do
     t.integer "phone"
     t.integer "like"
     t.string "pic"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "barber_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
