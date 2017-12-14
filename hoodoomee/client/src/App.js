53d879a1a7f9fdf2c6d4ebdcaa50f62885b2abab
Kareem James <kareemdjames@gmail.com>
2017-12-14 15:41:13 -0500

    created new react app frontend, able to connect to rails api

---
+++ hoodoomee/client/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            userData: null,
+            userDataReceived: false,
+        };
+    }
+
+    componentDidMount() {
+        fetch('/users')
+            .then(res => res.json())
+            .then(res => {
+                this.setState({
+                    userData: res,
+                    userDataReceived: true,
+                })
+            }).catch(err => console.log(err));
+    }
+
+    renderUsers() {
+        return this.state.userData.map((user) => {
+            return (
+                <div key={user.id}>
+                    <p>Name: {user.fname}</p>
+                    <p>Quote: {user.quote}</p>
+                </div>
+            );
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>I Am In React</h1>
+                { (this.state.userDataReceived)
+                    ? this.renderUsers()
+                    : <p>Loading...</p>
+                }
+            </div>
+        );
+    }
+}
+
+export default App;
