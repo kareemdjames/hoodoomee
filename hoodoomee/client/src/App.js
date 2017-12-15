6141ba34c2ca29e25d342db7fd5b23c814fc29b4
Kareem James <kareemdjames@gmail.com>
2017-12-15 10:20:24 -0500

    commented out auth not finished

--- hoodoomee/client/src/App.js
+++ hoodoomee/client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Auth from './Auth/Auth.js';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +9,23 @@ class App extends Component {
             userData: null,
             userDataReceived: false,
         };
+
+    // const auth = new Auth();
+    // auth.login();
     }
+
+    goTo(route) {
+        this.props.history.replace(`/${route}`)
+    }
+
+    login() {
+        this.props.auth.login();
+    }
+
+    logout() {
+        this.props.auth.logout();
+    }
+
 
     componentDidMount() {
         fetch('/users')
